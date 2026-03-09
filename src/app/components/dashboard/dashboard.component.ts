d5ae9b0b2aa6361ac065d6de81d20d6b9489b19a
CristianRivera03 <cr8539788@gmail.com>
2026-03-09 01:09:54 -0600

    feat: Enhance dashboard component with user session management and post loading functionality

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,53 @@
-import { Component,OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PostService } from '../../services/post.service';
+PostService
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
+
 export class DashboardComponent {
+
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private postService = inject(PostService);
+
+  //usuario actual
+  currentUser: any = null;
+  post: any[] = [];
+
+  postForm: FormGroup = this.fb.group({
+    content: ['', Validators.required, Validators.maxLength(500)]
+  });
+
+  ngOnInit() {
+    this.loadUserSession();
+    this.loadPost();
+  }
+
+  loadUserSession(){
+    const sessionData = localStorage.getItem("userSession");
+    if (sessionData){
+      this.currentUser = JSON.parse(sessionData);
+    }else{
+      this.router.navigate(["/login"]);
+    }
+  }
+
+  loadPost(){
+    this.postService.getAllPosts().subscribe(data => this.post = data);
+    console.log("cargando data");
+  }
+
+  
+
+
+
 
 }
